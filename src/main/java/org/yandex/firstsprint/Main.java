068459e03eb6bcdfaed3b58fee117f37ea275776
dginix <zero>
2022-06-30 10:12:01 +0300

    Чтение строки из файла и парсинг строк

--- src/main/java/org/yandex/firstsprint/Main.java
+++ src/main/java/org/yandex/firstsprint/Main.java
@@ -5,6 +5,62 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-    }
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Доступные команды: \n" +
+                "1. Считать все месячные отчеты\n" +
+                "2. Счиатать годовой отчет\n" +
+                "3. Сверить отчеты\n" +
+                "4. Вывести информацию о всех месячных отчетах\n" +
+                "5. Вывести информацию о годовом отчете\n" +
+                "0. Для выхода из программы\n");
+
+        System.out.println("Введите код команды:");
+
+        boolean continueFlg = true;
+
+        while(continueFlg){
+            if(scanner.hasNextInt()){
+                int userInput = scanner.nextInt();
+
+                switch (userInput){
+                    case 1:
+                        System.out.println("Считать все месячные отчеты...");
+                        String result = ParseData.readFileContent("data/m.202201.csv");
+                        ParseData.parseFileContent(result);
+                        break;
+
+                    case 2:
+                        System.out.println("Счиатать годовой отчет...");
+                        break;
+
+                    case 3:
+                        System.out.println("Сверить отчеты...");
+                        break;
+
+                    case 4:
+                        System.out.println("Вывести информацию о всех месячных отчетах...");
+                        break;
+
+                    case 5:
+                        System.out.println("Вывести информацию о годовом отчете...");
+                        break;
+
+                    case 0:
+                        continueFlg = false;
+                        System.out.println("Выход из программы...");
+                        break;
+
+                    default:
+                        System.out.println("Неизвестный код команды!");
+                        break;
+                }
+
+            }
+            else{
+                System.out.println("Введите корректный код команды!");
+                scanner.next();
+            }
+        }
+    }
 }
--- src/main/java/org/yandex/firstsprint/ParseData.java
+++ src/main/java/org/yandex/firstsprint/ParseData.java
@@ -1,7 +1,31 @@
 package org.yandex.firstsprint;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
 /**
- * @implNote Класс для парсинга данных из файла
+ * @implNote Класс для для чтения и парсинга данных из файла
  */
 public class ParseData {
+
+    static void parseFileContent(String content){
+        String[] lines = content.split("\\n");
+        for(String line : lines){
+            String[] lineContent = line.split(",");
+            System.out.println(lineContent);
+        }
+    }
+
+    static String readFileContent(String path){
+        try {
+            return Files.readString(Path.of(path));
+        } catch (IOException e){
+            System.out.println("Невозможно считать файл\n" +
+                    e);
+            return null;
+        }
+    }
+
 }
